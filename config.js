c0998b251afee94e40d02339103454a4ecfdd59a
nelsontobias <nelsonptobias@gmail.com>
2019-12-10 13:51:31 -0300

    trocando endereco do banco para funcionar no container

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const config = {
-  DB_URI: 'mongodb://localhost:27017/donamarialittlestore',
+  DB_URI: 'mongodb://mongodb:27017/donamarialittlestore',
   REQUIRED_FIELDS_PRODUCT: ['id', 'sku', 'name', 'price'],
   REQUIRED_FIELDS_CUSTOMER: ['id', 'name', 'cpf', 'email'],
   REQUIRED_FIELDS_ORDER: ['id', 'customer_id', 'total', 'status']
